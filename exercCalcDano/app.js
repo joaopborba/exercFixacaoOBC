3f3ef7630639bc741810087278ce4586cbeb2935
João Pedro Borba Pereira <joaopborba95@hotmail.com>
2023-04-18 18:29:13 -0300

    Finalizadno

--- exercCalcDano/app.js
+++ exercCalcDano/app.js
@@ -1,46 +1,20 @@
 const name1 = prompt("Digite o nome do personagem: ");
 const atk1 = prompt("Digite o poder de ataque desse personagem: ");
 const name2 = prompt("Digite o nome do segundo personagem: ");
-const life2 = prompt(
-  "Digite a quantidade de pontos de vida do(a) " + `${name2}:`
-);
-const def2 = prompt("Digite o poder de defesa do(a)" + `${name2}:`);
-const shield2 = prompt("O " + `${name2} tem escudo?`);
+let life2 = prompt("Digite a quantidade de pontos de vida do(a) " + name2);
+const def2 = prompt("Digite o poder de defesa do(a) " + name2);
+const shield = prompt("O " + name2 + " tem escudo? (sim/não)");
+let dano = 0;
 
-if (atk1 > def2 && shield2 === "não") {
-  let dano = atk1 - def2;
-  alert("O dano causado foi: " + dano);
-  let result = life2 - dano;
-  alert(
-    "Pontos de vida atual: " +
-      result +
-      `\nNome do atacante: ${name1}\n` +
-      `Poder de ataque: ${atk1}\n` +
-      `Nome do defensor: ${name2}\n` +
-      `Poder de defesa: ${def2}\n` +
-      `Pontos de vida: ${life2}`
-  );
-} else if (atk1 > def2 && shield2 === "sim") {
-  let dano = (atk1 - def2) / 2;
-  alert("O dano causado foi: " + dano);
-  let result = life2 - dano;
-  alert(
-    "Pontos de vida atual: " +
-      result +
-      `\nNome do atacante: ${name1}\n` +
-      `Poder de ataque: ${atk1}\n` +
-      `Nome do defensor: ${name2}\n` +
-      `Poder de defesa: ${def2}\n` +
-      `Pontos de vida: ${life2}`
-  );
-} else if (def2 >= atk1) {
-  alert("O dano foi 0");
-  alert(
-    "Pontos de vida atual: " +
-      life2 +
-      `\nNome do atacante: ${name1}\n` +
-      `Poder de ataque: ${atk1}\n` +
-      `Nome do defensor: ${name2}\n` +
-      `Poder de defesa: ${def2}\n`
-  );
+if (atk1 > def2 && shield === "não") {
+  dano = atk1 - def2;
+} 
+else if (atk1 > def2 && shield === "sim") {
+  dano = (atk1 - def2) / 2;
 }
+
+life2 -= dano;
+
+alert(name1 + " causou " + dano + " pontos de dano em " + name2);
+alert(name1 + "\nPoder de ataque: " + atk1 + "\n\n" +
+name2 + " \nPontos de vida: " + life2 + "\nPoder de defesa: " + def2 + "\nPossui escudo? " + shield);
