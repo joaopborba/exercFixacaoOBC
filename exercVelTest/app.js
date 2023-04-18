9eb53a119bb79f040e0b2103b5277604889aaa81
João Pedro Borba Pereira <joaopborba95@hotmail.com>
2023-04-18 17:37:50 -0300

    Finalizando exercício

--- exercVelTest/app.js
+++ exercVelTest/app.js
@@ -7,8 +7,10 @@ if (vel1 > vel2) {
   alert(
     "A velocidade do " + `${car1}` + " é superior a velocidade do " + `${car2}`
   );
-} else {
+} else if(vel2 > vel1){
   alert(
     "A velocidade do " + `${car2}` + " é superior a velocidade do " + `${car1}`
   );
+}else{
+  alert("A velocidade de ambos é a mesma!")
 }
