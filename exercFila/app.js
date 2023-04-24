21774b93c0ef1692127cf857ca5527b1185eb307
joaopborba <joaopborba95@hotmail.com>
2023-04-24 15:29:45 -0300

    Exercício corrigido

--- exercFila/app.js
+++ exercFila/app.js
@@ -1,21 +1,35 @@
 let lista = [];
 let menu = "";
 
-while (menu != "4") {
+while (menu != "3") {
+  let pacientes = "";
+  for (let i = 0; i < lista.length; i++) {
+    pacientes += "\n" + (i + 1) + "º - " + lista[i];
+  }
+
   menu = prompt(
-    "Escolha a opção desejada: \n1- Lista de pacientes\n2-Novo paciente\n3-Consultar paciente\n4- Sair"
+    "Lista de pacientes: " +
+      pacientes +
+      "\n\nEscolha a opção desejada: \n1-Novo paciente\n2-Consultar paciente\n3- Sair"
   );
 
   switch (menu) {
     case "1":
-      alert("Lista de pacientes: \n" + lista + "\n");
-      break;
-    case "2":
       let novopaciente = prompt("Digite o nome do paciente a ser adicionado: ");
       lista.push(novopaciente);
       break;
+    case "2":
+      if (lista.length <= 0) {
+        alert("Não tem paciente na fila!");
+      } else {
+        alert("Primeiro paciente da fila: \n" + lista.shift());
+      }
+      break;
     case "3":
-      alert("Primeiro paciente da fila: \n" + lista.shift());
+      alert("Finalizando...");
+      break;
+    default:
+      alert("Opção inválida!");
       break;
   }
 }
