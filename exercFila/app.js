487e95f0c9207cc3114d2c6d97d7bb445914e75d
joaopborba <joaopborba95@hotmail.com>
2023-04-24 14:43:15 -0300

    Exercício de Fila - 90%

---
+++ exercFila/app.js
@@ -0,0 +1,21 @@
+let lista = ["João", "Pedro", "Amanda"];
+let menu = "";
+
+while (menu != "4") {
+  menu = prompt(
+    "Escolha a opção desejada: \n1- Lista de pacientes\n2-Novo paciente\n3-Consultar paciente\n4- Sair"
+  );
+
+  switch (menu) {
+    case "1":
+      alert("Lista de pacientes: \n" + lista + "\n");
+      break;
+    case "2":
+      let novopaciente = prompt("Digite o nome do paciente a ser adicionado: ");
+      lista.push(novopaciente);
+      break;
+    case "3":
+      alert("Primeiro paciente da fila: \n" + lista.shift());
+      break;
+  }
+}
