3038f632c67d0edb3f68230fb142017137d80ce3
joaopborba <joaopborba95@hotmail.com>
2023-05-22 16:52:31 -0300

    Começando exercício

---
+++ exercCalcGeometrica/app.js
@@ -0,0 +1,34 @@
+let menu = "";
+
+function calcTri() {}
+
+while (menu != 6) {
+  menu = prompt(
+    "Escolha uma das opções: " +
+      "\n\n1-Área do triângulo" +
+      "\n2-Área do retângulo" +
+      "\n3-Área do quadrado" +
+      "\n4-Área do trapézio" +
+      "\n5-Área do circulo" +
+      "\n6-Sair"
+  );
+
+  switch (menu) {
+    case "1":
+      break;
+    case "2":
+      break;
+    case "3":
+      break;
+    case "4":
+      break;
+    case "5":
+      break;
+    case "6":
+      alert("Saindo do programa.");
+      break;
+    default:
+      alert("Escolha uma opção válida!");
+      break;
+  }
+}
