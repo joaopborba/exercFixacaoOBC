b13fb0aca3dca6a6d9a8727a06474e354a246301
joaopborba <joaopborba95@hotmail.com>
2023-05-29 10:25:57 -0300

    exercício completo

--- exercCalcGeometrica/app.js
+++ exercCalcGeometrica/app.js
@@ -1,6 +1,33 @@
 let menu = "";
 
-function calcTri() {}
+function calcTri() {
+  const base = prompt("Informe a base do triângulo: ");
+  const altura = prompt("Informe a altura do triângulo: ");
+  return (base * altura) / 2;
+}
+
+function calcRet() {
+  const base = prompt("Informe a base do retângulo: ");
+  const altura = prompt("Informe a altura do retângulo: ");
+  return base * altura;
+}
+
+function calcQuad() {
+  const lado = prompt("Informe o lado do quadrado: ");
+  return lado * lado;
+}
+
+function calcTrap() {
+  const bmenor = parseFloat(prompt("Informe a base menor do trapézio: "));
+  const bmaior = parseFloat(prompt("Informe a base maior do trapézio: "));
+  const altura = prompt("Informe a altura do trapézio: ");
+  return ((bmaior + bmenor) * altura) / 2;
+}
+
+function calcCirc() {
+  const raio = prompt("Informe o raio do círculo: ");
+  return 3.14 * raio * raio;
+}
 
 while (menu != 6) {
   menu = prompt(
@@ -15,14 +42,19 @@ while (menu != 6) {
 
   switch (menu) {
     case "1":
+      alert("Área do triângulo: " + calcTri());
       break;
     case "2":
+      alert("Área do retângulo: " + calcRet());
       break;
     case "3":
+      alert("Área do quadrado: " + calcQuad());
       break;
     case "4":
+      alert("Área do trapézio: " + calcTrap());
       break;
     case "5":
+      alert("Área do círculo: " + calcCirc());
       break;
     case "6":
       alert("Saindo do programa.");
