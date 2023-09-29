c9fe8ae8b2b68c1f3fec5e39a410972c9a818de1
joaopborba <joaopborba95@hotmail.com>
2023-09-29 12:24:40 -0300

    atualização

--- exercCadastroDevs/app.js
+++ exercCadastroDevs/app.js
@@ -1,6 +1,16 @@
-function createLabel (text, htmlFor){
-    const label = document.createElement('label');
-    label.htmlFor = htmlFor;
-    label.innerText = text;
-    return label;
+function createLabel(text, htmlFor) {
+  const label = document.createElement("label");
+  label.htmlFor = htmlFor;
+  label.innerText = text;
+  return label;
+}
+
+function createInput(id, name, value, type = "text", placeholder = "") {
+  const input = document.createElement("input");
+  input.id = id;
+  input.name = name;
+  input.value = value;
+  input.type = type;
+  input.placeholder = placeholder;
+  return input;
 }
